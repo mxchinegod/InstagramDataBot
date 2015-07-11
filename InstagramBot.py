275de9c478406a5d3f8ec83b3220c135d4e7b896
Dylan Moore <hosnappp@gmail.com>
2015-07-10 20:45:43 -0400

    Update InstagramBot.py
    100% working now.

--- InstagramBot.py
+++ InstagramBot.py
@@ -12,13 +12,14 @@ def init():
     if len(username) < 1:
         print "Not a valid username."
     else:
+        file(os.path.dirname(os.path.realpath(__file__)) + "instagram.txt", "w").write(urllib2.urlopen("http://www.instagram.com/" + username).read())
+        file(os.path.dirname(os.path.realpath(__file__)) + "followers.txt", "w")
         interval = raw_input("How often would you like to record(in seconds)?\n")
         if int(interval) < 60:
             opt = raw_input("That is very fast. You may experience decreased bandwidth on your network, continue? [Y/N]:\n")
-            if opt == ("Y"):
+            if opt == ("Y" or "y"):
                 print "Finished first export. Sleeping for " + interval + " seconds.\n"
                 while True:
-                    file(os.path.dirname(os.path.realpath(__file__)) + "instagram.txt", "w").write(urllib2.urlopen("http://www.instagram.com/" + username).read())
                     followers = str("followed_by\":{\"count\":")
                     search = open(os.path.dirname(os.path.realpath(__file__)) + "instagram.txt","r")
                     for line in search.readlines():
@@ -30,15 +31,14 @@ def init():
                                 documentprint = str((number, ":", datetime.datetime.now(),))
                                 chars_to_remove = ['(','\'',',','datetime.datetime',')']
                                 documentprint = documentprint.translate(None, ''.join(chars_to_remove))
-                                
-                    file(os.path.dirname(os.path.realpath(__file__)) + "followers.txt", "w").write(documentprint + "\n")
+                                with open(os.path.dirname(os.path.realpath(__file__)) + "followers.txt", "a") as f:
+                                    f.write(documentprint + '\n')
                     time.sleep(int(interval))
             else:
                 init()
         else:
             print "Finished first export. Sleeping for " + interval + " seconds.\n"
             while True:
-                file(os.path.dirname(os.path.realpath(__file__)) + "instagram.txt", "w").write(urllib2.urlopen("http://www.instagram.com/" + username).read())
                 followers = str("followed_by\":{\"count\":")
                 search = open(os.path.dirname(os.path.realpath(__file__)) + "instagram.txt","r")
                 for line in search.readlines():
@@ -50,6 +50,7 @@ def init():
                             documentprint = str((number, ":", datetime.datetime.now(),))
                             chars_to_remove = ['(','\'',',','datetime.datetime',')']
                             documentprint = documentprint.translate(None, ''.join(chars_to_remove))
-                file(os.path.dirname(os.path.realpath(__file__)) + "followers.txt", "w").write(documentprint + "\n")
+                            with open(os.path.dirname(os.path.realpath(__file__)) + "followers.txt", "a") as f:
+                                f.write(documentprint + '\n')
                 time.sleep(int(interval))
 init()
