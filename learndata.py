9277d907f4f92ffd7540f8230951f4d70cc37307
Dylan Moore <hosnappp@gmail.com>
2015-07-22 09:37:57 -0400

    Update learndata.py
    Made it more configurable. As I used it, the context of the graph
    changed rapidly and I needed to set it to different intervals and such
    each time. Now this prompts the user so the code is never adjusted.

--- learndata.py
+++ learndata.py
@@ -3,10 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+xaxis = int(raw_input("How long (minutes) is the data?: "))
+yaxis = int(raw_input("How tall (followers) is the data?: "))
+interval = int(raw_input("What was the interval (in seconds)?: "))
 data = []
 x = []
 y = []
-with open(os.getcwd() + '\data.txt', 'r') as f:
+with open(os.path.dirname(os.path.realpath(__file__)) + '\data.txt', 'r') as f:
     for line in f:
         data.append(line.split(':'))
 
@@ -18,8 +21,8 @@ for i in data:
 plt.title("Instagram")
 plt.xlabel("One Minute Intervals")
 plt.ylabel("Follower count relative to " + str(y[0]))
-t = np.arange(0., 1.0, 60.0)
+t = np.arange(0., 1.0, interval)
 plt.plot(y, 'r-')
 plt.grid()
-plt.axis([0, 2000, 4500, 6000])
+plt.axis([0, xaxis, yaxis/2, yaxis])
 plt.show()
